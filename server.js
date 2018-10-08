58cb25d8b40446c4c8a1e1543c9152687cc1a165
Joanne Kim <joanneskim831@gmail.com>
2018-10-08 11:50:05 -0700

    change db from redis to mysql

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const redis = require('redis');
 const db = require('./database/index');
 
 // const path = require('path');
@@ -12,7 +11,6 @@ const port = process.env.PORT || 8888;
 app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  db.set('string key', 'string val', redis.print);
   res.send('hello');
 });
 
